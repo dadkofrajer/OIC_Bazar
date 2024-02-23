f31f15bf1b121afd13306a8c05fb6513bbbae1c0
dadkofrajer <soboldado@gmail.com>
2024-02-23 13:45:00 +0000

    fixed issues

--- website/views.py
+++ website/views.py
@@ -8,7 +8,7 @@ from .models import User, Category, Listing
 
 
 def index(request):
-    return render(request, "website/index.html")
+    return render(request, "index.html")
 
 
 def login_view(request):
@@ -24,11 +24,11 @@ def login_view(request):
             login(request, user)
             return HttpResponseRedirect(reverse("index"))
         else:
-            return render(request, "website/login.html", {
+            return render(request, "login.html", {
                 "message": "Invalid username and/or password."
             })
     else:
-        return render(request, "website/login.html")
+        return render(request, "login.html")
 
 
 def logout_view(request):
@@ -45,7 +45,7 @@ def register(request):
         password = request.POST["password"]
         confirmation = request.POST["confirmation"]
         if password != confirmation:
-            return render(request, "website/register.html", {
+            return render(request, "register.html", {
                 "message": "Passwords must match."
             })
 
@@ -54,19 +54,19 @@ def register(request):
             user = User.objects.create_user(username, email, password)
             user.save()
         except IntegrityError:
-            return render(request, "website/register.html", {
+            return render(request, "register.html", {
                 "message": "Username already taken."
             })
         login(request, user)
         return HttpResponseRedirect(reverse("index"))
     else:
-        return render(request, "website/register.html")
+        return render(request, "register.html")
 
 #Creating a lisitng to sell
 def create(request):
     if request.method == "GET":
         categories = Category.objects.all()
-        return render("website/create.html", {"categories": categories})
+        return render("create.html", {"categories": categories})
     else:
         # Get the data from the form, about the user,
         #code
